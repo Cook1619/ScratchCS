e664b0c1249af4d31902aa8c5b9246d6354f0296
Cook1619 <cook6348@gmail.com>
2023-01-22 19:36:38 -0600

    inheritance example

---
+++ CSharpPlayground/Animal.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpPlayground
+{ 
+    // base class
+    public class Animal
+    {
+        public string name;
+
+        public void display()
+        {
+            Console.WriteLine("This is an animal");
+        }
+    }
+
+    // derived class of Animal
+    public class Dog : Animal
+    {
+        public void getName()
+        {
+            Console.WriteLine($"My name is {name}");
+        }
+    }
+}
+
+
+
